8dbb7fb6ace500c9e8f16d28bce7eb60f2c17bc9
Primera Prova <19ltusell@iesperealsius.cat>
2026-03-23 11:12:31 +0100

    TascaMenteniment

---
+++ src/prog2/model/TascaManteniment.java
@@ -0,0 +1,89 @@
+package prog2.model;
+
+public class TascaManteniment implements InTascaManteniment{
+
+    public static enum TipusTascaManteniment{
+        Reparacio,
+        Neteja,
+        RevisioTecnica,
+        Desinfeccio
+    }
+
+    private int num;
+    private TipusTascaManteniment tipus;
+    private Allotjament allotjament;
+    private String data;
+    private int dies;
+
+    public TascaManteniment(int num, TipusTascaManteniment tipus,
+                            Allotjament allotjament, String data, int dies){
+
+        this.num = num;
+        this.tipus = tipus;
+        this.allotjament = allotjament;
+        this.data = data;
+        this.dies = dies;
+
+    }
+
+    public  int getNum() {
+        return num;
+    }
+
+    public TascaManteniment.TipusTascaManteniment getTipus() {
+        return tipus;
+    }
+
+    public Allotjament getAllotjament(){
+        return allotjament;
+    }
+
+    public String getData(){
+        return data;
+    }
+
+    public int getDies(){
+        return dies;
+    }
+
+    public void setNum(int num){
+        this.num = num;
+    }
+
+    public void setTipus(TipusTascaManteniment tipus){
+        this.tipus = tipus;
+    }
+
+    @Override
+    public void setAllotjament(Allotjament allotjament) {
+        this.allotjament = allotjament;
+    }
+
+    @Override
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    @Override
+    public void setDies(int dies) {
+        this.dies = dies;
+    }
+
+    @Override
+    public String getIluminacioAllotjament() {
+        switch (tipus)
+            {
+            case Reparacio: return "50%";
+            case RevisioTecnica: return "50%";
+            case Desinfeccio: return "0%";
+            case Neteja: return "100%";
+            default: return null;
+            }
+
+    }
+
+    @Override
+    public String toString() {
+        return "Tasca " + num + " - " + tipus + " - " + allotjament.getNom() + " - " + data + " - " + dies + " dies";
+    }
+}
