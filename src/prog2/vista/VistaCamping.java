9ab9c43b76acfa713c5e5f5d09c33e91963feb69
Primera Prova <19ltusell@iesperealsius.cat>
2026-03-24 13:57:18 +0100

    Menu1

--- src/prog2/vista/VistaCamping.java
+++ src/prog2/vista/VistaCamping.java
@@ -1,26 +1,97 @@
 package prog2.vista;
 
-
+import java.util.Scanner;
 import prog2.model.Camping;
 
 public class VistaCamping {
 
+    private static enum OpcionsMenu {
+        LLISTAR_ALLOTJAMENTS,
+        LLISTAR_OPERATIUS,
+        LLISTAR_NO_OPERATIUS,
+        LLISTAR_ACCESSOS_OBERTS,
+        LLISTAR_ACCESSOS_TANCATS,
+        LLISTAR_TASQUES,
+        AFEGIR_TASCA,
+        COMPLETAR_TASCA,
+        ACCESSOS_SENSE_VEHICLE,
+        METRES_ACCESSOS_TERRA,
+        GUARDAR,
+        CARREGAR,
+        SORTIR
+    }
+
+    private static String[] descripcions = {
+            "Llistar tots els allotjaments",
+            "Llistar allotjaments operatius",
+            "Llistar allotjaments no operatius",
+            "Llistar accessos oberts",
+            "Llistar accessos tancats",
+            "Llistar tasques de manteniment actives",
+            "Afegir tasca de manteniment",
+            "Completar tasca de manteniment",
+            "Nombre d'accessos sense vehicle",
+            "Metres totals d'accessos de terra",
+            "Guardar càmping",
+            "Carregar càmping",
+            "Sortir"
+    };
+
     private Camping camping;
-    public static enum llistaOpcions{
-        Llistar_la_informació_de_tots_els_allotjaments, Llistar_la_informació_dels_allotjaments_operatius,
-        Llistar_la_informació_dels_allotjaments_no_operatius, Llistar_la_informació_dels_accessos_oberts,
-        Llistar_la_informació_dels_accessos_tancats, Llistar_la_informació_de_les_tasques_de_manteniments_actives,
-        Afegir_una_tasca_de_manteniment, Completar_una_tasca_de_manteniment,
-        Calcular_i_mostrar_el_número_total_daccessos_que_NO_proporcionen_accessibilitat_amb_vehicle
-    }
-    public VistaCamping(Camping camping) {
 
-        this.camping = camping;
-
+    public VistaCamping(String nomCamping) {
+        camping = new Camping(nomCamping);
+        camping.inicialitzaDadesCamping();
     }
 
-    public void gestioCamping(){
+    public void gestioCamping() {
+        Scanner sc = new Scanner(System.in);
+        Menu<OpcionsMenu> menu = new Menu<OpcionsMenu>("Menu Càmping Green", OpcionsMenu.values());
+        menu.setDescripcions(descripcions);
 
-        Menu menu = new Menu(llistaOpcions);
+        OpcionsMenu opcio = menu.getOpcio(sc);
+        while(opcio != OpcionsMenu.SORTIR) {
+            menu.mostrarMenu();
+            switch(opcio) {
+                case LLISTAR_ALLOTJAMENTS:
+
+                    break;
+                     case LLISTAR_OPERATIUS:
+
+                         break;
+                         case LLISTAR_NO_OPERATIUS:
+
+                             break;
+                             case LLISTAR_ACCESSOS_OBERTS:
+
+                                 break;
+                                 case LLISTAR_ACCESSOS_TANCATS:
+
+                                     break;
+                                     case LLISTAR_TASQUES:
+
+                                         break;
+                                         case AFEGIR_TASCA:
+
+                                             break;
+                                             case COMPLETAR_TASCA:
+
+                                                 break;
+                                                 case METRES_ACCESSOS_TERRA:
+
+                                                     break;
+                                                     case GUARDAR:
+
+                                                         break;
+                                                         case CARREGAR:
+
+                                                             break;
+                case SORTIR:
+
+                    System.out.println("Fins aviat!");
+                    break;
+            }
+            opcio = menu.getOpcio(sc);
+        }
     }
 }
